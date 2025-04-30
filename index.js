87f0e2fa70ad67d949b108b6f5e9cf9d7dd38365
Denis <c.bodoaga.3490@vallauri.edu>
2025-04-30 11:06:01 +0200

    test 1 + implementazione celle

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const DIM = 4;
 
 window.onload=function(){
 
+
+   const wrapper = document.getElementById("wrapper");
+
+   for(let i=0;i<DIM;i++)
+   {
+      for(let j=0;j<DIM;j++)
+      {
+         const div = document.createElement("div");
+         div.id = `div-${i}-${j}`;
+         div.classList.add("cella");
+         wrapper.appendChild(div);
+      }
+   }
 }
 
 function generaNumero(min, max){
